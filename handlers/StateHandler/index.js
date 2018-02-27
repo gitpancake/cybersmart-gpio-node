80a1a174d7482ba132b1045259e1b4e26d880ad8
Henry <henrypye@outlook.com>
2018-02-27 14:20:13 +0000

    Added redundancy incase node not running on Raspberry Pi

--- handlers/StateHandler/index.js
+++ handlers/StateHandler/index.js
@@ -1,18 +1,29 @@
 /*jshint esversion: 6*/
-var GPIO = require('onoff').Gpio;
+var pi = false;
+
+try {
+    var GPIO = require('onoff').Gpio;
+    pi = true;
+} catch (ex) {
+    console.log('[WARNING] RUN THIS ON A RASPBERRY PI TO USE GPIO');
+}
 
 var SWITCH = [];
 var pins = [7];
 
 class StateHandler {
     constructor() {
-        for(var pin in pins) {
-            SWITCH.push(new GPIO(pin, 'out'));
+        if (pi) {
+            for(var pin in pins) {
+                SWITCH.push(new GPIO(pin, 'out'));
+            }
+            console.log(SWITCH);
         }
-        console.log(SWITCH);
     }
 
     ChangeState(state) {
-        
+        console.log(state);
     }
 }
+
+module.exports = StateHandler;
