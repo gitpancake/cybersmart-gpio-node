ae8994a0d104773a25d2c1324f9e8cf70c2809a4
Henry Pye <henrypye@outlook.com>
2018-03-11 16:19:25 +0000

    Added more stable on/off functionality

--- handlers/StateHandler/on.py
+++ handlers/StateHandler/on.py
@@ -1,8 +1,12 @@
 import RPi.GPIO as GPIO
 import sys
+
 GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BOARD)             
 GPIO.setup(7, GPIO.OUT) # PIN 7 - our pin
 
 GPIO.output(7, 1) 
+
+print "on"
+sys.stdout.flush()
