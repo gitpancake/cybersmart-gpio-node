b539d1f9e1b7d46c0b8fc1c5c542f978790b5063
Henry <henrypye@outlook.com>
2018-04-26 23:43:00 +0100

    Started writing Changer for state...oh my

--- handlers/StateHandler/index.js
+++ handlers/StateHandler/index.js
@@ -1,30 +1,19 @@
 /*jshint esversion: 6*/
 var pi = false;
-var GPIO = require('rpi-gpio');
 
 var SWITCH = [];
 var pins = [7];
 
 class StateHandler {
     constructor() {
-        try {
-            GPIO.setup(7, GPIO.DIR_OUT, write);
-        } catch (ex) {
-            console.log('[WARNING] RUN THIS ON A RASPBERRY PI TO USE GPIO');
-        }
+        
     }
 
     ChangeState(state) {
         for (let pin in pins) {
-            GPIO.write(7, (state === 1 ? true : false), function(err) {
-                if (err) 
-                    console.error(err);
-                
-                console.log("Written to pin");
-            });
+            
         }   
     }
 }
-
 
 module.exports = StateHandler;
